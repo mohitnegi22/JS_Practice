d9e437abbcfb8c014d7df012b008b2844ed5c2ac
Mohit Negi <72944509+mohitnegi22@users.noreply.github.com>
2023-09-05 16:49:54 +0000

    newCOmmit

---
+++ Math/fun.js
@@ -0,0 +1,4 @@
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min+1))+min);
+console.log(Math.floor(Math.random()*10)+1)
---
+++ Math/time.js
@@ -0,0 +1,12 @@
+let myDate=new Date
+console.log(myDate.toDateString());
+console.log(myDate.toISOString());
+console.log(myDate.toJSON());
+console.log(myDate.toLocaleString());
+console.log(myDate.toString());
+
+let newDate=new Date();
+console.log(newDate)
+
+let XYZ= Date.now()
+console.log(XYZ)
